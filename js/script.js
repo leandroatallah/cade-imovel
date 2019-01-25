1af318a2f54bb7f8b37d29468a2a713b671aef4c
Leandro Atallah <leandro.atallah@artedigitalinternet.com.br>
2019-01-24 22:15:06 -0200

    Ajustes de cor primária | Responsivo

--- js/script.js
+++ js/script.js
@@ -10,13 +10,40 @@ $(document).ready(function(){
 	$('.list-imovel.list-relac').slick({
 		slidesToShow: 3,
 		slidesToScroll: 1,
+		responsive: [{
+	      breakpoint: 1024,
+	      settings: {
+	        slidesToShow: 1,
+	        slidesToScroll: 1,
+	        arrows: false,
+  			autoplay: true,
+  			autoplaySpeed: 2500,
+	      }
+	    }]
+	});
+
+	$('.filter, #close-btn').on('click', function() {
+		$('.aside-content').fadeToggle(300);
+	});
+
+	$('.aside-content h3').on('click', function() {
+		if($(this).next('ul').is(':visible')) {
+			$('h3 + ul').slideUp();
+		} else {
+			$('h3 + ul').slideUp();
+			$(this).next('ul').slideDown('open');
+		}
 	});
 });
 
 function toggleFormat(f) {
 	if(f == 'col') {
-		$('.list-imovel').addClass('format-col');
+		$('.list-imovel').addClass('format-col').find('.thumb-list').slick('refresh');
 	} else {
-		$('.list-imovel').removeClass('format-col');
+		$('.list-imovel').removeClass('format-col').find('.thumb-list').slick('refresh');
 	}
+}
+
+function toggleInfoBox(e) {
+	jQuery('.' + e + '-box').slideToggle(300);
 }
